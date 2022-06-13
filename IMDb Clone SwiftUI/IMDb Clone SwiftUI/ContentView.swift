b0d08ee8a1d3c4d82bad95deec8504d8bffb6c46
Saran Goda <saran@quintessential.design>
2022-06-13 18:41:01 +0530

    Initial Commit

---
+++ IMDb Clone SwiftUI/IMDb Clone SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IMDb Clone SwiftUI
+//
+//  Created by Quin Design on 6/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IMDb Clone SwiftUI/IMDb Clone SwiftUI/IMDb_Clone_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDb_Clone_SwiftUIApp.swift
+//  IMDb Clone SwiftUI
+//
+//  Created by Quin Design on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct IMDb_Clone_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
